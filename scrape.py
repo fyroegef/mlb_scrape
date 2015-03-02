983f9f90d64c770a19881805e69409acc85c4c75
geoff <fyroegef@gmail.com>
2015-03-01 17:01:43 -0800

    game_data format to dict

--- scrape.py
+++ scrape.py
@@ -13,10 +13,10 @@ class gameScraper(object):
 			dt_temp = datetime.now()
 			self.year,self.month,self.day = dt_temp.strftime('%Y'),dt_temp.strftime('%m'),dt_temp.strftime('%d')
 		self.schedule = self._getSchedule()
-		self.game_datas = []
+		self.game_datas = {}
 		if fetch:
 			for game_id in self.schedule:
-				self.game_datas.append(self._getXML(game_id))
+				self.game_datas[game_id] = self._getXML(game_id)
 
 	def _getSchedule(self):
 		## Semi-private method to lookup games on a particular day
@@ -32,14 +32,19 @@ class gameScraper(object):
 		## Semi-private method to lookup a game's xml and convert it into a list/dictionary structure that is default to ElementTree
 		game_path = self.path + 'gid_' + game_id + '/inning/inning_all.xml'
 		print game_path
-		req = urllib2.Request(game_path)
-		pbp_xml = urllib2.urlopen(req).read().split('-->')[1]
-		game_data = et.fromstring(pbp_xml)
-		return game_data
+		try:
+			req = urllib2.Request(game_path)
+			pbp_xml = urllib2.urlopen(req).read().split('-->')[1]
+			game_data = et.fromstring(pbp_xml)
+			return game_data
+		except:
+			return None
 
 def pitchFX(game_data,game_id):
 	## Converts the ElementTree default structure into a list of all pitchF/X info for the whole game
 	## all units are converted to MKS
+	if not game_data:
+		return None
 	home_id = game_id.split('_')[-2][:3]
 	away_id = game_id.split('_')[-3][:3]
 	timestamp = ''.join(game_id.split('_')[:3])
@@ -51,30 +56,32 @@ def pitchFX(game_data,game_id):
 				if item.tag == 'atbat':
 					for subitem in item:
 						if subitem.tag == 'pitch':
+							try:
+								pitch_temp = {}
+								pitch_temp['batter_id'] = item.attrib['batter']
+								pitch_temp['pitcher_id'] = item.attrib['pitcher']
+								pitch_temp['timestamp'] = timestamp
+								pitch_temp['home_id'] = home_id
+								pitch_temp['away_id'] = away_id
+								pitch_temp['inning_num'] = i+1
+								if subitem.attrib['type'] == 'X':
+									pitch_temp['result'] = item.attrib['event']
+								else:
+									pitch_temp['result'] = None
+								pitch_temp['call'] = subitem.attrib['type']
 
-							pitch_temp = {}
-							pitch_temp['batter_id'] = item.attrib['batter']
-							pitch_temp['pitcher_id'] = item.attrib['pitcher']
-							pitch_temp['timestamp'] = timestamp
-							pitch_temp['home_id'] = home_id
-							pitch_temp['away_id'] = away_id
-							pitch_temp['inning_num'] = i+1
-							if subitem.attrib['type'] == 'X':
-								pitch_temp['result'] = item.attrib['event']
-							else:
-								pitch_temp['result'] = None
-							pitch_temp['call'] = subitem.attrib['type']
+								pitch_temp['spd'] = float(subitem.attrib['start_speed']) * 0.447
+								pitch_temp['sz'] = (float(subitem.attrib['sz_bot'])*0.3048,float(subitem.attrib['sz_top'])*0.3048)
+								pitch_temp['pos_i'] = (float(subitem.attrib['x0'])*0.3048,float(subitem.attrib['y0'])*0.3048,float(subitem.attrib['z0'])*0.3048)
+								pitch_temp['pos_f'] = (float(subitem.attrib['px'])*0.3048,0,float(subitem.attrib['pz'])*0.3048)
+								pitch_temp['ddt'] = (float(subitem.attrib['pfx_x'])*0.0254,0,float(subitem.attrib['pfx_z'])*0.0254)
+								pitch_temp['y_mxbr'] = float(subitem.attrib['break_y'])*0.3048
+								pitch_temp['br_ang'] = float(subitem.attrib['break_angle'])*np.pi/180
+								pitch_temp['br_len'] = float(subitem.attrib['break_length'])*0.0254
 
-							pitch_temp['spd'] = float(subitem.attrib['start_speed']) * 0.447
-							pitch_temp['sz'] = (float(subitem.attrib['sz_bot'])*0.3048,float(subitem.attrib['sz_top'])*0.3048)
-							pitch_temp['pos_i'] = (float(subitem.attrib['x0'])*0.3048,float(subitem.attrib['y0'])*0.3048,float(subitem.attrib['z0'])*0.3048)
-							pitch_temp['pos_f'] = (float(subitem.attrib['px'])*0.3048,0,float(subitem.attrib['pz'])*0.3048)
-							pitch_temp['ddt'] = (float(subitem.attrib['pfx_x'])*0.0254,0,float(subitem.attrib['pfx_z'])*0.0254)
-							pitch_temp['y_mxbr'] = float(subitem.attrib['break_y'])*0.3048
-							pitch_temp['br_ang'] = float(subitem.attrib['break_angle'])*np.pi/180
-							pitch_temp['br_len'] = float(subitem.attrib['break_length'])*0.0254
-
-							fx_data.append(pitch_temp)
+								fx_data.append(pitch_temp)
+							except:
+								continue
 	return fx_data
 
 
